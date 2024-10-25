1c47b5e20b83f0975cb29b29b06d7d1d08865fa6
Can Özkaymak <can.ozkaymak@qpbilisim.com.tr>
2024-10-26 00:22:54 +0300

    third

--- MerchanterApp/MerchanterApp.AppHost/Program.cs
+++ MerchanterApp/MerchanterApp.AppHost/Program.cs
@@ -5,7 +5,4 @@ builder.AddProject<Projects.MerchanterApp_ApiService>( "MerchanterApi" );
 builder.AddProject<Projects.Merchanter_ServerService>( "ServerService" );
 
 
-builder.AddProject<Projects.MerchanterApp_Frontend_Server>("merchanterapp-frontend-server");
-
-
 builder.Build().Run();
