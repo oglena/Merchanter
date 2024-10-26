ce306a88ec8208ef6608c30ea6ca6698b70afbf2
Can Özkaymak <can.ozkaymak@qpbilisim.com.tr>
2024-10-26 06:33:06 +0300

    26102024_3

--- Merchanter.ServerService/Classes/MerchanterServer.cs
+++ Merchanter.ServerService/Classes/MerchanterServer.cs
@@ -1,6 +1,9 @@
-﻿namespace Merchanter.ServerService.Classes {
+﻿using Merchanter.Classes;
+
+namespace Merchanter.ServerService.Classes {
     public class MerchanterServer {
         public int PID { get; set; }
         public int customer_id { get; set; }
+        public Customer? customer { get; set; }
     }
 }
--- Merchanter.ServerService/Repositories/ServerRepository.cs
+++ Merchanter.ServerService/Repositories/ServerRepository.cs
@@ -6,9 +6,12 @@ using System.Management;
 namespace Merchanter.ServerService.Repositories {
     public interface IServerRepository {
         Task<List<MerchanterServer>> GetWorkingServers();
+        MerchanterService merchanterService { get; set; }
     }
-    public class ServerRepository() :IServerRepository {
+    public class ServerRepository( MerchanterService merchanterService ) :IServerRepository {
         public List<MerchanterServer> working_servers = new List<MerchanterServer>();
+
+        MerchanterService IServerRepository.merchanterService { get; set; } = new MerchanterService();
 
         public async Task<List<MerchanterServer>> GetWorkingServers() {
             return await GetServers();
@@ -19,12 +22,11 @@ namespace Merchanter.ServerService.Repositories {
                 working_servers = new List<MerchanterServer>() { new MerchanterServer() { customer_id = 0, PID = 0 } };
                 return working_servers;
             } );
-            var processes = Process.GetProcesses();
+            var processes = Process.GetProcessesByName( "MerchanterServer" );
             List<MerchanterServer> servers = new List<MerchanterServer>();
             foreach( var item in processes ) {
-                if( item.ProcessName == "MerchanterServer" ) {
-                    if( int.TryParse( GetCustomerId( item ), out int cid ) )
-                        servers.Add( new MerchanterServer() { PID = item.Id, customer_id = cid } );
+                if( int.TryParse( GetCustomerId( item ), out int cid ) ) {
+                    servers.Add( new MerchanterServer() { PID = item.Id, customer_id = cid, customer = merchanterService.helper.GetCustomer( cid ) } );
                 }
             }
             return servers;
@@ -33,7 +35,7 @@ namespace Merchanter.ServerService.Repositories {
         private string GetCustomerId( Process item ) {
             using( ManagementObjectSearcher searcher = new ManagementObjectSearcher( "SELECT CommandLine FROM Win32_Process WHERE ProcessId = " + item.Id ) )
             using( ManagementObjectCollection objects = searcher.Get() ) {
-                return objects.Cast<ManagementBaseObject>().SingleOrDefault()?[ "CommandLine" ]?.ToString().Split('"')[2].Trim();
+                return objects.Cast<ManagementBaseObject>().SingleOrDefault()?[ "CommandLine" ]?.ToString().Split( '"' )[ 2 ].Trim();
             }
         }
     }
